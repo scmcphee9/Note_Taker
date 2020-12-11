32ae9293ab75fa8f1b16b6dd2ad12dae67902a1a
Shawn McPhee <scmcphee9@gmail.com>
2020-12-10 21:57:56 -0700

    made changes to apiroutes get file reading the file, and added express.static to server

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -17,14 +17,15 @@ module.exports = (app) => {
   app.get("/api/notes", (req, res) => {
     // const newNote = req.body;
     // console.log(newNote);
-    res.send(
-      fs.readFile("./db/db.json", "utf8", (error, data) =>
-        error ? console.error(error) : console.log(data)
-      )
+
+    fs.readFile("./db/db.json", "utf8", (error, data) =>
+      error ? console.error(error) : res.send(data)
     );
 
     //   fs readfile for returning
   });
 
-  app.post("/api/notes", (req, res) => {});
+  app.post("/api/notes", (req, res) => {
+    // read file, make new array, write new file and send!
+  });
 };
--- Develop/server.js
+++ Develop/server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 require("./routes/htmlRoutes")(app);
 require("./routes/apiRoutes")(app);
