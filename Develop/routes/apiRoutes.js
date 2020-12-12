081fcc26b0f4696670f971d48764d6af2f3de91c
Shawn McPhee <scmcphee9@gmail.com>
2020-12-12 14:25:06 -0700

    added unique id to post route.  started creating a delete route

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,6 +1,11 @@
 const path = require("path");
 const fs = require("fs");
-const id = require("unique-identity");
+const { v4: uuidv4 } = require("uuid");
+const util = require("util");
+
+const readfileasync = util.promisify(fs.readFile);
+const writefileasync = util.promisify(fs.writeFile);
+
 // const db = require("./db/db.json");
 
 // const notes = require("db.json"); how do we pull or connect to database file
@@ -15,17 +20,54 @@ module.exports = (app) => {
   //   );
 
   app.get("/api/notes", (req, res) => {
-    // const newNote = req.body;
-    // console.log(newNote);
-
     fs.readFile("./db/db.json", "utf8", (error, data) =>
       error ? console.error(error) : res.send(data)
     );
-
-    //   fs readfile for returning
   });
 
   app.post("/api/notes", (req, res) => {
-    // read file, make new array, write new file and send!
+    // let newNote;
+    readfileasync("./db/db.json", "utf8")
+      .then((currentNotes) => {
+        let current = JSON.parse(currentNotes);
+
+        const newNote = {
+          title: req.body.title,
+          text: req.body.text,
+          id: uuidv4(),
+        };
+
+        const updatedNotes = [...current, newNote];
+
+        console.log(updatedNotes);
+
+        return updatedNotes;
+      })
+      .then((data) => {
+        writefileasync("./db/db.json", JSON.stringify(data));
+      });
   });
+
+  // app.delete("/api/notes",(req, res) => {
+  //   readfileasync("./db/db.json", "utf8")
+  //   .then((currentNotes) => {
+  //     let current = JSON.parse(currentNotes);
+
+  //     const newNote = {
+  //       title: req.body.title,
+  //       text: req.body.text,
+  //       id: uuidv4(),
+  //     };
+
+  //     const updatedNotes = [...current, newNote];
+
+  //     console.log(updatedNotes);
+
+  //     return updatedNotes;
+  //   })
+  //   .then((data) => {
+  //     writefileasync("./db/db.json", JSON.stringify(data));
+  //   });
+
+  // })
 };
